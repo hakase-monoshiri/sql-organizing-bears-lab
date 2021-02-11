485d00a34f86535f014fb92caa3fea29ffd0a02a
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-02-10 20:36:33 -0500

    commit tree fed1ab002fff9a4e6a0f0dbc55ab0dc063e23869

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "friendly", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Black", "friendly", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 12, "F", "Brown", "shy", "false");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Black", "friendly", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "confident", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "M", "Panda", "lazy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", "energetic", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 7, "M", "Polar", "mean", null);
